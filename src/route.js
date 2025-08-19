dfc98589cdcd87fb88982eff030be34bb63bd3e3
Maria Eduarda <mariaed.cassiano@gmail.com>
2025-08-19 20:47:05 -0300

    first commit

---
+++ src/route.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
+import Header from './components/Header';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sobre" element={<Sobre />} />
+                <Route path="/contato" element={<Contato />} />
+            </Routes>
+
+        </BrowserRouter>
+    )
+}
+
+export default Router;
