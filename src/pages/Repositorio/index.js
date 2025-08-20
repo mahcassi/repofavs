70351f513dc5d59290097a36047cc26ef4ef5a08
Maria Eduarda <mariaed.cassiano@gmail.com>
2025-08-20 09:04:52 -0300

    Carregando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,12 +1,42 @@
 import { useParams } from 'react-router-dom';
+import { Container } from './styles';
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
 
-export default function Repositorio({match}) {
+export default function Repositorio() {
     const { repo } = useParams();
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        async function load() {
+            const nomeRepo = decodeURIComponent(repo);
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepositorio(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+    }, []);
+
+
 
     return (
-        <div>
-            <h1 style={{color: '#FFFF'}}>repo Page</h1>
-           {decodeURIComponent(repo)}
-        </div>
+        <Container>
+            <h1 style={{ color: '#FFFF' }}>repo Page</h1>
+            {decodeURIComponent(repo)}
+        </Container>
     );
 }
